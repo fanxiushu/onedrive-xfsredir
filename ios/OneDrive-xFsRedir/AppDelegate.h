c2edd8ec85e3e692d39eca0a567c1fcfc7dcbab3
Fanxiushu <fanxiushu@163.com>
2018-05-04 06:38:07 +0800

    xfsredir files system uploaded

---
+++ ios/OneDrive-xFsRedir/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneDrive-xFsRedir
+//
+//  Created by fanxiushu on 2018/4/17.
+//  Copyright © 2018年 fanxiushu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
